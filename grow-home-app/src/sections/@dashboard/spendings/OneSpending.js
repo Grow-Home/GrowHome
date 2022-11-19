df163d117be623001c0a3f6091c52e4039142ba5
Sarah.Berbuir <livine.gmks@gmail.com>
2022-11-19 19:13:17 +0100

    Spending page

---
+++ grow-home-app/src/sections/@dashboard/spendings/OneSpending.js
@@ -0,0 +1,49 @@
+import PropTypes from 'prop-types';
+// @mui
+import { Box, Card, Link, Typography, Stack } from '@mui/material';
+import { styled } from '@mui/material/styles';
+// utils
+import { fCurrency } from '../../../utils/formatNumber';
+// components
+import Label from '../../../components/label';
+import { ColorPreview } from '../../../components/color-utils';
+
+// ----------------------------------------------------------------------
+
+const StyledProductImg = styled('img')({
+  top: 0,
+  width: '100%',
+  height: '100%',
+  objectFit: 'cover',
+  position: 'absolute',
+});
+
+// ----------------------------------------------------------------------
+/*
+OneSpending.propTypes = {
+  product: PropTypes.object,
+};
+//const { name, cover, price, colors, status, priceSale } = product;
+*/
+export default function OneSpending() {
+  
+
+  return (
+    <Card>
+      <Box sx={{ pt: '100%', width:'100%', position: 'relative' }}>
+
+          <Label
+            variant="filled"
+            color={'info'}
+
+          >
+            Category
+          </Label>
+        
+        <p>Hallo</p>
+      </Box>
+
+   
+    </Card>
+  );
+}
---
+++ grow-home-app/src/sections/@dashboard/spendings/SpendingList.js
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+// @mui
+import { Grid } from '@mui/material';
+import OneSpending from './OneSpending';
+
+// ----------------------------------------------------------------------
+
+SpendingList.propTypes = {
+  products: PropTypes.array.isRequired,
+};
+
+export default function SpendingList({ products}) {
+  return (
+    <Grid container spacing={3} >
+      {products.map((product) => (
+        <Grid item xmd={6} xs={12} >
+            <OneSpending style={{height: '100%', width: '30px'}} product={product}/>
+        </Grid>
+          
+        
+      ))}
+    </Grid>
+  );
+}
